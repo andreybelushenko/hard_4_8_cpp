1ad1b1d489d2592f399fd4ec9b8279873422529c
andreybelushenko <dfreeman@ukr.net>
2014-05-31 22:56:17 +0300

    ошибка в строках 41-48 проблема со стеком массива f_check_nodes

--- eigen_lib_for_ldpc/my_ldpc_4__8.cpp
+++ eigen_lib_for_ldpc/my_ldpc_4__8.cpp
@@ -5,9 +5,13 @@
 using namespace std;
 
 int matr_vec_mult(int h_check_matrix [4][8], int y_received_codeword[8]);
+///int funk1_for_f_check_nodes(int h_check_matrix [4][8], int c_bit_nodes[8]);
+
 
 int main()
 {
+	int m=4;
+	int n=8;
 	int y_received_codeword[8] = {1, 1, 0, 1, 0, 1, 0, 1};// {1, 0, 0, 1, 0, 1, 0, 1}
 	int h_check_matrix [4][8] =
 	{
@@ -20,26 +24,43 @@ int main()
 	int iteration_counter = 0;
 	
 	int sindrom = matr_vec_mult(h_check_matrix, y_received_codeword);
+	
+	int c_bit_nodes[8];
+	int f_check_nodes[4][4];
 
-	while (sindrom != 0 && iteration_counter < 5)
+	for (int i = 0; i < 8; i++)
+	{
+		c_bit_nodes[i] = y_received_codeword[i];  
+	}
+
+
+	while (sindrom != 0 && iteration_counter < 3)
 	{
 		iteration_counter = iteration_counter + 1;
+	
+	for  (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			if (h_check_matrix[i][j] == 1)
+				f_check_nodes[i][j]= c_bit_nodes[j];			
+		}
+			}
 
-		cout << "y_received_codeword =  " << y_received_codeword[8] << endl;
 	}
-	cout << "sindrom=  " << sindrom << endl;
-	cout << "h_check_matrix=  " << h_check_matrix << endl;
+
 	cin.get();
 	return 0;
-}
+	}
+
 
 int matr_vec_mult(int h_check_matrix [4][8], int y_received_codeword[8])
-
+{
 	// a[m][n]						h_check_matrix
 	// b[] - vect n  elem			y_received_codeword
 	// c[] - vect m  elem
 
-{
+
 	
 	int c[4] = {1, 1, 1, 1};
 	int m = 4;
@@ -58,4 +79,62 @@ int matr_vec_mult(int h_check_matrix [4][8], int y_received_codeword[8])
 
 return sum_sindrom;
 }
-///
+
+//int funk1_for_f_check_nodes(int h_check_matrix [4][8], int c_bit_nodes[8])
+//{
+//	int m = 4;
+//	int n = 8;
+//	int index = 0;
+//	int f_check_nodes[4][4];
+//
+//	for  (int i = 0; i < m; i++)
+//	{
+//		for (int j = 0; j < n; j++)
+//		{
+//			if (h_check_matrix[i][j] != 0)
+//			index = h_check_matrix[i][j];
+//			f_check_nodes[i][j]= c_bit_nodes[index];
+//		
+//		}
+//
+//
+//	}
+//
+//	
+//	//cout << "\n  f_check_nodes=  \n";// вывод f матрицы
+//	//for (int i = 0; i < 4; i++)
+//	//	{
+//	//		for (int j = 0; j < 4; j++)
+//	//		{
+//	//		cout << f_check_nodes[i][j];
+//	//		}
+//	//	cout <<endl;
+//	//	}///////////////////////////////////////////////////////
+//
+//}
+
+
+
+
+
+
+
+//	cout << "y_received_codeword =  " ;//вывод кодового слова
+	//	for (int i = 0; i < 8; i++)
+	//	{
+	//		cout << y_received_codeword[i];
+	//	}
+	//	cout << endl;
+	//}
+	//cout << "sindrom=  " << sindrom << endl;//////////////////////
+
+
+	//
+	//cout << "h_check_matrix=  \n";// вывод проверочной матрицы
+	//for (int i = 0; i < 4; i++)
+	//	{
+	//		for (int j = 0; j < 8; j++)
+	//		{
+	//		cout << h_check_matrix[i][j];
+	//		}
+	//	cout <<endl;
