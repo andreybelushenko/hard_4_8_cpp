d64159725aaa892f204e93d4ab87cfc6d7daacde
andreybelushenko <dfreeman@ukr.net>
2014-06-01 16:59:06 +0300

    problem with stack

--- eigen_lib_for_ldpc/my_ldpc_4__8.cpp
+++ eigen_lib_for_ldpc/my_ldpc_4__8.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int matr_vec_mult(int h_check_matrix [4][8], int y_received_codeword[8]);
-
+//void print_matrix (const int row, const int column, const int matrix[4][4] );
 int main()
 {
 	int y_received_codeword[8] = {1, 1, 0, 1, 0, 1, 0, 1};// {1, 0, 0, 1, 0, 1, 0, 1}
@@ -19,6 +19,7 @@ int main()
 
 	int c_bit_nodes[8];
 	int f_check_nodes[4][4];
+	int parity_check [4][4];
 
 	for (int i = 0; i < 8; i++)
 	{
@@ -30,31 +31,60 @@ int main()
 	{
 		for  (int i = 0; i < 4; i++)
 		{
+			int k = 0;
 			for (int j = 0; j < 8; j++)
 			{
 				if (h_check_matrix[i][j] == 1)
-					cout<< (f_check_nodes[i][j] = c_bit_nodes[j]);	
-				
+				{	
+					k++;
+					f_check_nodes[i][k] = c_bit_nodes[j];	
+					cout<<f_check_nodes[i][k];
+				}
 			}
 			cout << endl;
 		}
 		cout << endl;  cout << endl;
+
+
+
+		// формируем матрицу parity_check [4][4];
+		for  (int i = 0; i < 4; i++)
+		{
+			int p = 0;
+			for (int j = 0; j < 4; j++)
+			{
+				for (int k = 0; k < 4; j++)
+				{
+					
+					p = p + f_check_nodes[i][k];
+				}
+				p = p - f_check_nodes[i][j];
+				p = (p % 2);
+				parity_check[i][j] = p;
+				cout << parity_check[i][j];
+			}
+			cout << endl;
+		}
+		cout << endl;  cout << endl;
+					  
+
+					
+
+
+
+
+
+
+
+
+
 	}
 
 
 
 
 		
-	cout << "f_check_matrix=  \n";// вывод f матрицы
-	for (int i = 0; i < 4; i++)
-		{
-			for (int j = 0; j < 4; j++)
-			{
-			cout << f_check_nodes[i][j];
-			}
-		cout << endl;
-		}
-	cout << "!!!!!!!!!!!!!!!!!!=!!!  \n";
+	/*print_matrix(4,4,f_check_nodes);*/
 
 
 
@@ -77,16 +107,33 @@ int matr_vec_mult(int h_check_matrix [4][8], int y_received_codeword[8])
 	int n = 8;
 	int sum_sindrom = 0;
 
-	for (int i = 0; i < m; i++)  {
+	for (int i = 0; i < m; i++) 
+	{
 		c[i] = 0;
-		for (int j =0; j < n; j++){
+		for (int j =0; j < n; j++)
+		{
 			c[i] += h_check_matrix[i][j]  *  y_received_codeword[j];
 		}
 	}
 
 	for (int i = 0; i < m; i++)
+	{
 		sum_sindrom = sum_sindrom + (c[i] % 2);
-
+	}
 	return sum_sindrom;
 }
 
+//void print_matrix (const int row, const int column, const int matrix[4][4] )
+//{
+//		cout << "matrix=  \n";
+//	for (int i = 0; i < row; i++)
+//		{
+//			for (int j = 0; j < column; j++)
+//			{
+//			cout << matrix[i][j];
+//			}
+//		cout <<endl;
+//
+//}
+//}
+//
